652350f0476c4370ad9d7a2e8700c2a0b000dc1b
Daniel Koenigsperger <36309503+dkoenigs@users.noreply.github.com>
2020-02-15 21:12:26 -0500

    Update mishmash.clj

--- src/mishmash.clj
+++ src/mishmash.clj
@@ -1,140 +1,148 @@
-(ns mishmash
-  (:require [clojure.string :as str]))
+(ns idiot (:import (java.io File))
+    (:require [clojure.java.io :as io])
+    (:import java.security.MessageDigest)
+    (:import java.util.zip.DeflaterOutputStream
+      (java.io ByteArrayInputStream ByteArrayOutputStream))
+    (:import java.io.ByteArrayOutputStream
+      java.util.zip.InflaterInputStream))
 
-;Calculates factorial of value >= 0
-(defn factorial [value] (
-                          if (= value 0)
-                          1
-                          (* value (factorial (- value 1)))
-                          )
-  )
+;--------------------------------------------------| PROVIDED HELPER FUNCTIONS
+(defn sha1-hash-bytes [data]
+      (.digest (MessageDigest/getInstance "sha1")
+               (.getBytes data)))
 
-;For each line of the row 'value' of pascal's triangle, calculates binomial coefficient
-;Either adds a " " or a "\n" when at the end
-(defn pascal [value] (
-                       if (>= value 0)
-                       (dotimes [r (+ value 1)]
-                         (if (= r value)
-                           (print (str (/ (factorial value) (* (factorial r) (factorial (- value r)))) "\n"))
-                           (print (str (/ (factorial value) (* (factorial r) (factorial (- value r)))) " "))
-                           )
-                         )
-                       (println "invalid input")
-                       )
-  )
+(defn byte->hex-digits [byte]
+      (format "%02x"
+              (bit-and 0xff byte)))
 
-;Storing Roman Numerals for each multiple of 10
-(def m ["" "M" "MM" "MMM"])
-(def c ["" "C" "CC" "CCC" "CD" "D" "DC" "DCC" "DCCC" "CM"])
-(def x ["" "X" "XX" "XXX" "XL" "L" "LX" "LXX" "LXXX" "XC"])
-(def i ["" "I" "II" "III" "IV" "V" "VI" "VII" "VIII" "IX"])
-(def roman-letters "MCDXLIV")
-(def read-m 0)
-(def read-c 0)
-(def read-x 0)
-(def read-i 0)
+(defn bytes->hex-string [bytes]
+      (->> bytes
+           (map byte->hex-digits)
+           (apply str)))
 
+(defn sha1-sum [header+blob]
+      (bytes->hex-string (sha1-hash-bytes header+blob)))
 
+(defn zip-str
+      "Zip the given data with zlib. Return a ByteArrayInputStream of the zipped
+      content."
+      [data]
+      (let [out (ByteArrayOutputStream.)
+            zipper (DeflaterOutputStream. out)]
+           (io/copy data zipper)
+           (.close zipper)
+           (ByteArrayInputStream. (.toByteArray out))))
 
-(defn write-roman [value] (
-                            if (and (>= value 1) (< value 4000))
-                            (do
-                              (def tho (get m (quot value 1000)))
-                              (def hun (get c (quot (rem value 1000) 100)))
-                              (def ten (get x (quot (rem value 100) 10)))
-                              (def one (get i (rem value 10)))
-                              (println (str tho (str hun (str ten one))))
-                              )
-                            (println "invalid input")
-                            )
-  )
+(defn unzip
+      "Unzip the given data with zlib. Pass an opened input stream as the arg. The
+      caller should close the stream afterwards."
+      [input-stream]
+      (with-open [unzipper (InflaterInputStream. input-stream)
+                  out (ByteArrayOutputStream.)]
+                 (io/copy unzipper out)
+                 (->> (.toByteArray out)
+                      (map char)
+                      (apply str))))
 
-(defn read-roman [value] (do
+;--------------------------------------------------| HASH-OBJECT HELPER
+(defn hash-object-helper [switch, file-name] (do
+                                               (def header-and-blob (str "blob " (count (slurp file-name)) "\000" (slurp file-name)))
+                                               (def address (sha1-sum header-and-blob))
+                                               (if (= "-w" switch)
+                                                 (do (def dir1 (subs address 0 2))
+                                                     (def dir2 (subs address 2))
+                                                     (.mkdir (File. (str ".git/objects/" dir1)))
+                                                     (io/copy (zip-str header-and-blob) (io/file (str ".git/objects/" dir1 "/" dir2)))
+                                                     (println address))
+                                                 (println address)
+                                                 )
+                                               ))
 
-                           (doseq [i (range 1 4)]
-                             (if (.contains value (get m i))
-                               (def read-m i)
-                               )
-                             )
-                           ;Ensure that we are not accidentally reading a subtraction
-                           (if (and (= read-m 1) (.contains value "CM"))
-                             (if (and (= read-m 1) (.contains value "MCM")) ;Check whether subtraction is intentional
-                               ()
-                               (def read-m 0)
-                               )
-                             )
-
-                           (doseq [i (range 1 10)]
-                             (if (.contains value (get c i))
-                               (if (<= (count (get c read-c)) (count (get c i)))
-                                 (def read-c i))
-                               )
-                             )
-                           ;Ensure that we are not accidentally reading a subtraction
-                           (if (and (= read-c 1) (.contains value "XC"))
-                             (if (and (= read-c 1) (.contains value "CXC")) ;Check whether subtraction is intentional
-                               ()
-                               (def read-c 0)
-                               )
-                             )
-
-                           (doseq [i (range 1 10)]
-                             (if (.contains value (get x i))
-                               (if (<= (count (get x read-x)) (count (get x i)))
-                                 (def read-x i))
-                               )
-                             )
-                           ;Ensure that we are not accidentally reading a subtraction
-                           (if (and (= read-x 1) (.contains value "IX"))
-                             (if (and (= read-x 1) (.contains value "XIX")) ;Check whether subtraction is intentional
-                               ()
-                               (def read-x 0)
-                               )
-                             )
-
-                           (doseq [j (range 1 10)]
-                             (if (.contains value (get i j))
-                               (if (<= (count (get i read-i)) (count (get x j)))
-                                 (def read-i j))
-                               )
-                             )
-
-                           (println (+ (* 1000 read-m) (+ (* 100 read-c) (+ (* 10 read-x) read-i))))
-                           )
-  )
-
-
-
-(defn -main [& value]
-  (if (= 2 (count value))
-    (cond
-      (= (nth value 0) "pascal")  (try
-                                    (pascal (Integer/parseInt (nth value 1)))
-                                    (catch NumberFormatException e (println "invalid input"))
-                                    )
-
-
-      (= (nth value 0) "write-roman") (try
-                                        (write-roman (Integer/parseInt (nth value 1)))
-                                        (catch NumberFormatException e (println "invalid input"))
-                                        )
-
-      (= (nth value 0) "read-roman") (if (string? (nth value 1))
-                                       (if (= true (boolean (re-find #"^[I|X|C|M|D|L|V]*$" (nth value 1)))) ; Verify Roman alphabet
-                                         (if (= true (boolean (re-find #"(IIII+)|(XXXX+)|(CCCC+)|(MMMM+)|(DD+)|(LL+)|(VV+)" (nth value 1)))) ; Verify correct number of roman characters
-                                           (println "invalid input")
-                                           (if (= true (boolean (re-find #"IXXC|IXCM|XCCM|IIXC|IL|ID|IM|IC|XD|XM" (nth value 1))))
-                                             ;Checking for invalid order of roman numerals and invalid subtractions
-                                             (println "invalid input")
-                                             (read-roman (nth value 1))
-                                             )
-                                           )
-                                         (println "invalid input")
-                                       )
-                                       (println "invalid input")
-                                       )
-      :else         (println "invalid input")
+;--------------------------------------------------| HELP
+(defn help [arg]
+      (do
+        (cond
+          (= 0 (count arg)) (println "idiot: the other stupid content tracker\n\nUsage: idiot <command> [<args>]\n\nCommands:\n   help\n   init\n   hash-object [-w] <file>\n   cat-file -p <address>")
+          (or  (= "help" (nth arg 0)) (= "--help" (nth arg 0)) (= "-h" (nth arg 0))) (println "idiot help: print help for a command\n\nUsage: idiot help <command>\n\nArguments:\n   <command>   the command to print help for\n\nCommands:\n   help\n   init\n   hash-object [-w] <file>\n   cat-file -p <address>")
+          (= "init" (nth arg 0)) (println "idiot init: initialize a new database\n\nUsage: idiot init\n\nArguments:\n   -h   print this message")
+          (= "hash-object" (nth arg 0)) (println "idiot hash-object: compute address and maybe create blob from file\n\nUsage: idiot hash-object [-w] <file>\n\nArguments:\n   -h       print this message\n   -w       write the file to database as a blob object\n   <file>   the file")
+          (= "cat-file" (nth arg 0)) (println "idiot cat-file: print information about an object\n\nUsage: idiot cat-file -p <address>\n\nArguments:\n   -h          print this message\n   -p          pretty-print contents based on object type\n   <address>   the SHA1-based address of the object")
+          :else (println "Error: invalid command")
+          )
+        )
       )
-    (println "invalid input")
+
+;--------------------------------------------------| INIT
+(defn init [arg]
+      (do
+        (cond
+          (and (= 1 (count arg)) (or  (= "-h" (nth arg 0)) (= "--help" (nth arg 0)))) (println "idiot init: initialize a new database\n\nUsage: idiot init\n\nArguments:\n   -h   print this message")
+          (< 0 (count arg)) (println "Error: init accepts no arguments")
+          :else (if (.isDirectory (io/file ".git"))
+                  (println "Error: .git directory already exists")
+                  (do (.mkdir (File. ".git")) (.mkdir (File. ".git/objects")) (println "Initialized empty Idiot repository in .git directory"))
+                  )
+          )
+        )
+      )
+
+;--------------------------------------------------| HASH-OBJECT
+(defn hash-object [arg]
+      (do
+        (cond
+          (and (= 1 (count arg)) (or  (= "-h" (nth arg 0)) (= "--help" (nth arg 0)))) (println "idiot hash-object: compute address and maybe create blob from file\n\nUsage: idiot hash-object [-w] <file>\n\nArguments:\n   -h       print this message\n   -w       write the file to database as a blob object\n   <file>   the file")
+          (not (.isDirectory (io/file ".git"))) (println "Error: could not find database. (Did you run `idiot init`?)")
+          (and (= 2 (count arg)) (= "-w" (nth arg 0))) (
+                                                         if (.isFile (io/file (nth arg 1)))
+                                                         (hash-object-helper "-w" (nth arg 1))
+                                                         (println "Error: that file isn't readable")
+                                                         )
+          (and (= 1 (count arg)) (not (= "-w" (nth arg 0)))) (
+                                    if (.isFile (io/file (nth arg 0)))
+                                    (hash-object-helper "" (nth arg 0))
+                                    (println "Error: that file isn't readable")
+                                    )
+          :else (println "Error: you must specify a file.")
+          )
+        )
+      )
+
+;--------------------------------------------------| CAT-FILE
+(defn cat-file [arg]
+      (do
+        (cond
+          (and (= 1 (count arg)) (or  (= "-h" (nth arg 0)) (= "--help" (nth arg 0)))) (println "idiot cat-file: print information about an object\n\nUsage: idiot cat-file -p <address>\n\nArguments:\n   -h          print this message\n   -p          pretty-print contents based on object type\n   <address>   the SHA1-based address of the object")
+          (not (.isDirectory (io/file ".git"))) (println "Error: could not find database. (Did you run `idiot init`?)")
+          (and (= 2 (count arg)) (= "-p" (nth arg 0))) (do
+                                                         (def blob-location (str ".git/objects/" (subs (nth arg 1) 0 2) "/" (subs (nth arg 1) 2)))
+                                                         (if (.isFile (io/file blob-location))
+                                                           (do
+                                                             (def unzipped-blob (with-open [input (-> blob-location io/file io/input-stream)]
+                                                                                 (unzip input)))
+                                                           (def start-of-blob (.indexOf unzipped-blob "\000"))
+                                                           (print (subs unzipped-blob (+ start-of-blob 1)))
+                                                             )
+                                                           (println "Error: that address doesn't exist")
+                                                           )
+                                                   )
+          (< 0 (count arg)) (println "Error: you must specify an address")
+          :else (println "Error: the -p switch is required"))
+        )
+      )
+
+
+
+
+;--------------------------------------------------| MAIN
+(defn -main [& value]
+  (do
+    (cond
+      (or (= 0 (count value)) (= "-h" (nth value 0)) (= "--help" (nth value 0))) (help (drop 1 value))
+      (= "help" (nth value 0)) (help (drop 1 value))
+      (= "init" (nth value 0)) (init (drop 1 value))
+      (= "hash-object" (nth value 0)) (hash-object (drop 1 value))
+      (= "cat-file" (nth value 0)) (cat-file (drop 1 value))
+      :else (println "Error: invalid command")
+      )
     )
   )
