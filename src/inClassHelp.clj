2f20183c37c4ae54ee6885e558b88b3ce55109ba
Daniel Koenigsperger <36309503+dkoenigs@users.noreply.github.com>
2020-03-20 18:05:08 -0400

    Delete inClassHelp.clj

--- src/inClassHelp.clj
+++
@@ -1,8 +0,0 @@
-(ns inClassHelp
-    require '[clojure.java.io :as io])
-
-(defrecord FileSystemEntry
-           [type parent-path name contents])
-
-(->FileSystemEntry :file "src" "inClassHelp.clj" "<contents>")
-
